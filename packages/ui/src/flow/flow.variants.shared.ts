de1cb60b06c742469aab4f0ebb631568ad20c164
dnnsjsk <dnnsjsk@gmail.com>
2026-03-03 17:04:03 +0100

    refactor(flow): remove lg spacing overrides

--- packages/ui/src/flow/flow.variants.shared.ts
+++ packages/ui/src/flow/flow.variants.shared.ts
@@ -1,2 +1,2 @@
-export const sharedFlowStyles = 'flex flex-col gap-2.5 lg:gap-4'
-export const sharedFlowButtonSpacingStyles = 'mt-4 lg:mt-6'
+export const sharedFlowStyles = 'flex flex-col gap-2.5'
+export const sharedFlowButtonSpacingStyles = 'mt-4'
